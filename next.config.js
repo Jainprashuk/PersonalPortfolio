ab6f82a9070ec5febf873434ab34acc1d0e69ff8
Prashuk Jain <prashukjain@Prashuks-MacBook-Air.local>
2025-10-15 23:30:33 +0530

    integrapting pwa app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
 const nextConfig = {}
 
-module.exports = nextConfig
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
